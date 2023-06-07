379eb2c1b9b13c90b03891eb96a2f7442632a1ae
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-06-07 23:14:33 +0900

    solidity version

--- contracts/community/Ticket.sol
+++ contracts/community/Ticket.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
